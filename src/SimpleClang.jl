4e66be106c67059ca1c1a5433f1bbd069c50be51
Benoît Legat <benoit.legat@gmail.com>
2026-02-17 15:30:31 +0100

    Add code from last year's lectures

--- src/SimpleClang.jl
+++ src/SimpleClang.jl
@@ -1,5 +1,207 @@
 module SimpleClang
 
-greet() = print("Hello World!")
+import Clang_jll
+import MultilineStrings
+import InteractiveUtils
+
+abstract type Code end
+
+struct CCode <: Code
+    code::String
+end
+
+macro c_str(s)
+    return :($CCode($(esc(s))))
+end
+
+struct CppCode <: Code
+    code::String
+end
+
+macro cpp_str(s)
+    return :($CppCode($(esc(s))))
+end
+
+struct CLCode <: Code
+    code::String
+end
+
+macro cl_str(s)
+    return :($CLCode($(esc(s))))
+end
+
+source_extension(::CCode) = "c"
+source_extension(::CppCode) = "cpp"
+source_extension(::CLCode) = "cl"
+
+compiler(::CCode, mpi::Bool) = mpi ? "mpicc" : "clang"
+function compiler(::CppCode, mpi::Bool)
+    @assert !mpi
+    return "clang++"
+end
+
+inline_code(code::AbstractString, ext::String) = HTML("""<code class="language-$ext">$code</code>""")
+inline_code(code::Code) = inline_code(code.code, source_extension(code))
+
+function md_code(code::AbstractString, ext::String)
+    code = "```" * ext * '\n' * code
+    if code[end] != '\n'
+        code *= '\n'
+    end
+    return Markdown.parse(code * "```")
+end
+md_code(code::Code) = md_code(code.code, source_extension(code))
+function Base.show(io::IO, m::MIME"text/html", code::Code)
+    return show(io, m, md_code(code))
+end
+
+function compile(
+    code::Code;
+    lib,
+    emit_llvm = false,
+    cflags = ["-O3"],
+    mpi::Bool = false,
+    use_system::Bool = mpi || "-fopenmp" in cflags, # `-fopenmp` will not work with pure Clang_jll, it needs openmp installed as well
+    verbose = 0,
+)
+    path = mktempdir()
+    main_file = joinpath(path, "main." * source_extension(code))
+    bin_file = joinpath(path, ifelse(emit_llvm, "main.llvm", ifelse(lib, "lib.so", "bin")))
+    write(main_file, code.code)
+    args = String[]
+    if !use_system && code isa CppCode
+        # `clang++` is not part of `Clang_jll`
+        push!(args, "-x")
+        push!(args, "c++")
+    end
+    append!(args, cflags)
+    if lib
+        push!(args, "-fPIC")
+        push!(args, "-shared")
+    end
+    if emit_llvm
+        push!(args, "-S")
+        push!(args, "-emit-llvm")
+    end
+    push!(args, main_file)
+    push!(args, "-o")
+    push!(args, bin_file)
+    try
+        if use_system
+            cmd = Cmd([compiler(code, mpi); args])
+            if verbose >= 1
+                @info("Compiling : $cmd")
+            end
+            run(cmd)
+        else
+            Clang_jll.clang() do exe
+                cmd = Cmd([exe; args])
+                if verbose >= 1
+                    @info("Compiling : $cmd")
+                end
+                run(cmd)
+            end
+        end
+    catch err
+        if err isa ProcessFailedException
+            return
+        else
+            rethrow(err)
+        end
+    end
+    return bin_file
+end
+
+function emit_llvm(code; kws...)
+    llvm = compile(code; lib = false, emit_llvm = true, kws...)
+    InteractiveUtils.print_llvm(stdout, read(llvm, String))
+    return code
+end
+
+function compile_lib(code::Code; kws...)
+    return codesnippet(code), compile(code; lib = true, kws...)
+end
+
+function compile_and_run(code::Code; verbose = 0, args = String[], valgrind::Bool = false, mpi::Bool = false, num_processes = nothing, show_run_command = !isempty(args) || verbose >= 1, kws...)
+    bin_file = compile(code; lib = false, mpi, verbose, kws...)
+    if !isnothing(bin_file)
+        cmd_vec = [bin_file; args]
+        if valgrind
+            cmd_vec = ["valgrind"; cmd_vec]
+        end
+        if mpi
+            if !isnothing(num_processes)
+                cmd_vec = [["-n", string(num_processes)]; cmd_vec]
+            end
+            cmd_vec = ["mpiexec"; cmd_vec]
+        end
+        cmd = Cmd(cmd_vec)
+        if show_run_command
+            @info("Running : $cmd") # `2:end-1` to remove the backsticks
+        end
+        try
+            run(cmd)
+        catch err
+            @warn(string(typeof(err)))
+        end
+    end
+    return codesnippet(code)
+end
+
+function wrap_in_main(content)
+    code = content.code
+    if code[end] == '\n'
+        code = code[1:end-1]
+    end
+    return typeof(content)("""
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+$(MultilineStrings.indent(code, 2))
+}
+""")
+end
+
+function wrap_compile_and_run(code; kws...)
+    compile_and_run(wrap_in_main(code); kws...)
+    return code
+end
+
+# TODO It would be nice if the user could select a dropdown or hover with the mouse and see the full code
+function codesnippet(code::Code)
+    lines = readlines(IOBuffer(code.code))
+    i = findfirst(line -> contains(line, "codesnippet"), lines)
+    if isnothing(i)
+        return code
+    end
+    j = findlast(line -> contains(line, "codesnippet"), lines)
+    return typeof(code)(join(lines[i+1:j-1], '\n'))
+end
+
+struct Example
+    name::String
+end
+
+function code(example::Example)
+    code = read(joinpath(dirname(dirname(dirname(@__DIR__))), "examples", example.name), String)
+    ext = split(example.name, '.')[end]
+    if ext == "c"
+        return CCode(code)
+    elseif ext == "cpp" || ext == "cc"
+        return CppCode(code)
+    elseif ext == "cl"
+        return CLCode(code)
+    else
+        error("Unrecognized extension `$ext`.")
+    end
+end
+
+function compile_and_run(example::Example; kws...)
+    return compile_and_run(code(example); kws...)
+end
+
+function compile_lib(example::Example; kws...)
+    return compile_lib(code(example); kws...)
+end
 
 end # module SimpleClang
